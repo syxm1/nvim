3b9ad402d8f2a61644d16ba791965bcdcc5b8c8d
syxm1 <hisyamprsetyo@gmail.com>
2025-03-30 17:29:51 +0700

    changes

--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -1,22 +1,40 @@
 return {
 	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
+
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		"nvim-tree/nvim-web-devicons",
 		"MunifTanjim/nui.nvim",
 	},
+
 	lazy = false,
-	---@module "neo-tree"
-	---@type neotree.Config?
+
 	opts = {},
 	config = function()
 		local tree = require("neo-tree")
+
 		tree.setup({
+			popup_border_style = "rounded",
 			window = {
 				position = "float",
+				width = 35,
+			},
+			event_handlers = {
+				{
+					event = "file_renamed",
+					handler = function(args)
+						print(args.source, " renamed to ", args.destination)
+					end,
+				},
+				{
+					event = "file_moved",
+					handler = function(args)
+						print(args.source, " moved to ", args.destination)
+					end,
+				},
 			},
 		})
+
 		vim.keymap.set("n", "<leader>e", ":Neotree<CR>", {})
 	end,
 }
