6639d13dce91bf09e61ee9df78aa0586dfa3520b
Hisyam Prasetyo <106479331+syxm1@users.noreply.github.com>
2025-03-30 16:56:54 +0700

    Update tree.lua

--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -1,63 +1,22 @@
 return {
-	"nvim-tree/nvim-tree.lua",
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
 	dependencies = {
+		"nvim-lua/plenary.nvim",
 		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
 	},
-
-	event = "VimEnter",
-
-	init = function()
-		vim.g.loaded_netrw = 1
-		vim.g.loaded_netrwPlugin = 1
-		vim.opt.termguicolors = true
-	end,
-
+	lazy = false,
+	---@module "neo-tree"
+	---@type neotree.Config?
+	opts = {},
 	config = function()
-		local function on_attach(bufnr)
-			local api = require("nvim-tree.api")
-			api.config.mappings.default_on_attach(bufnr)
-		end
-
-    vim.keymap.set("n", "<leader>e", ":NvimTreeOpen<CR>", { desc = "Toggle File Tree" })
-
-		require("nvim-tree").setup({
-			sort = { sorter = "case_sensitive" },
-			view = {
-				width = 38,
-				side = "right",
+		local tree = require("neo-tree")
+		tree.setup({
+			window = {
+				position = "float",
 			},
-			renderer = {
-				highlight_opened_files = "all",
-				highlight_git = true,
-				root_folder_modifier = ":t",
-				indent_markers = { enable = true },
-				icons = {
-					show = { git = true, folder = true, file = true, folder_arrow = true },
-					glyphs = {
-						default = "",
-						symlink = "",
-						git = {
-							unstaged = "",
-							staged = "✓",
-							unmerged = "",
-							renamed = "➜",
-							untracked = "",
-							deleted = "",
-							ignored = "◌",
-						},
-						folder = {
-							default = "",
-							open = "",
-							empty = "",
-							empty_open = "",
-							symlink = "",
-						},
-					},
-				},
-				group_empty = true,
-			},
-			filters = { dotfiles = false },
-			on_attach = on_attach,
 		})
+		vim.keymap.set("n", "<leader>e", ":Neotree<CR>", {})
 	end,
 }
