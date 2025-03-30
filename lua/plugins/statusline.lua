463bac3d0eb2da07507d53b508e64bd4f97d281c
syxm1 <hisyamprsetyo@gmail.com>
2025-03-30 18:54:45 +0700

    changes

--- lua/plugins/statusline.lua
+++ lua/plugins/statusline.lua
@@ -1,74 +1,68 @@
 return {
-  "nvim-lualine/lualine.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  event = "VeryLazy",
-  config = function()
-    local clients_lsp = function()
-      local bufnr = vim.api.nvim_get_current_buf()
-
-      local clients = vim.lsp.get_clients()
-      if next(clients) == nil then
-        return ""
-      end
-
-      local c = {}
-      for _, client in pairs(clients) do
-        table.insert(c, client.name)
-      end
-      return " " .. table.concat(c, "|")
-    end
-
-    require("lualine").setup({
-      options = {
-        theme = "catppuccin-macchiato",
-        component_separators = "",
-        section_separators = { left = "", right = "" },
-        disabled_filetypes = { "alpha", "Outline" },
-      },
-      sections = {
-        lualine_a = {
-          { "mode", separator = { left = " ", right = "" }, icon = "" },
-        },
-        lualine_b = {
-          {
-            "filetype",
-            icon_only = true,
-            padding = { left = 1, right = 0 },
-          },
-          "filename",
-        },
-        lualine_c = {
-          {
-            "branch",
-            icon = "",
-          },
-          {
-            "diff",
-            symbols = { added = " ", modified = " ", removed = " " },
-            colored = false,
-          },
-        },
-        lualine_x = {
-          {
-            "diagnostics",
-            symbols = { error = " ", warn = " ", info = " ", hint = " " },
-            update_in_insert = true,
-          },
-        },
-        lualine_y = { clients_lsp },
-        lualine_z = {
-          { "location", separator = { left = "", right = " " }, icon = "" },
-        },
-      },
-      inactive_sections = {
-        lualine_a = {},
-        lualine_b = {},
-        lualine_c = {},
-        lualine_x = {},
-        lualine_y = {},
-        lualine_z = {},
-      },
-      extensions = {},
-    })
-  end,
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	event = "VeryLazy",
+	config = function()
+		require("lualine").setup({
+			options = {
+				theme = "catppuccin-macchiato",
+				component_separators = "",
+				section_separators = { left = "", right = "" },
+				disabled_filetypes = { "alpha", "Outline" },
+			},
+			sections = {
+				lualine_a = {
+					{
+						"mode",
+						icon = "",
+						separator = { left = "", right = "" },
+						padding = { left = 0, right = 0 },
+					},
+				},
+				lualine_b = {
+					{
+						"buffers",
+					},
+				},
+				lualine_c = {
+					{
+						"branch",
+						icon = "",
+					},
+					{
+						"diff",
+						symbols = {
+							added = " ",
+							modified = " ",
+							removed = " ",
+						},
+					},
+				},
+				lualine_x = {
+					{
+						"diagnostics",
+						symbols = { error = " ", warn = " ", info = " ", hint = " " },
+						update_in_insert = true,
+					},
+				},
+				lualine_y = {
+					{
+						"lsp_status",
+					},
+				},
+				lualine_z = {
+					{ "location", separator = { left = "", right = " " }, icon = "" },
+				},
+			},
+			inactive_sections = {
+				lualine_a = {},
+				lualine_b = {},
+				lualine_c = {},
+				lualine_x = {},
+				lualine_y = {},
+				lualine_z = {},
+			},
+			extensions = {},
+		})
+	end,
 }
