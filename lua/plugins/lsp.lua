32f6ca06c004e95659f5699dcdcc8658eeba1401
syxm1 <hisyamprsetyo@gmail.com>
2025-03-30 13:08:42 +0700

    tidying things

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -25,15 +25,34 @@ return {
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 			lspconfig.lua_ls.setup({ capabilities = capabilities })
-      lspconfig.ts_ls.setup({ capabilities = capabilities })
+			lspconfig.ts_ls.setup({ capabilities = capabilities })
 			lspconfig.clangd.setup({ capabilities = capabilities })
 			lspconfig.pylyzer.setup({ capabilities = capabilities })
-      lspconfig.rust_analyzer.setup({ capabilities = capabilities })
+			lspconfig.rust_analyzer.setup({ capabilities = capabilities })
 
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
+
+	{
+		"nvimtools/none-ls.nvim",
+		event = "VimEnter",
+		config = function()
+			local null = require("null-ls")
+
+			null.setup({
+				sources = {
+					null.builtins.formatting.stylua,
+					null.builtins.formatting.prettier,
+					null.builtins.formatting.black,
+					null.builtins.formatting.clang_format,
+				},
+			})
+
+			vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
 		end,
 	},
 }
